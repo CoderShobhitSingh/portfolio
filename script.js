f76fed3ef0804b464a6cf08b740b5dd7f84a97a0
CoderShobhitSingh <snghshobhit@gmail.com>
2025-03-29 10:37:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function showSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+  }
+  function hideSidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+  }
+  var typed = new Typed('#element', {
+    strings: ['Web Developer.', 'Web Desiner','&amp; Graphic Designer.'],
+    typeSpeed: 50,
+  });
+
+  function linkedAcc(){
+    window.location.href="https://www.linkedin.com/in/shobhit-singh-51940a267/"
+  }
